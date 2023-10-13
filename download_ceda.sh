a2a040525a18cac0e80d71cb64d95d19d47cc335
PamelaOpio <120655188+PamelaOpio@users.noreply.github.com>
2023-10-13 10:10:45 +0100

    Change to ssp370

--- download_ceda.sh
+++ download_ceda.sh
@@ -4,7 +4,7 @@
 #
 
 for model in "CAS/FGOALS-g3" "MIROC/MIROC6" "NCAR/CESM2-WACCM" "AWI/AWI-CM-1-1-MR"; do
-    for ssp in "ssp126" "ssp245" "ssp460" "ssp585"; do
+    for ssp in "ssp126" "ssp245" "ssp370" "ssp585"; do
         for variable in "tas" "pr"; do
             echo "SSP: $ssp  VAR: $variable"
             echo "wget -e robots=off --mirror --no-parent --no-clobber -r https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/$model/$ssp/r1i1p1f1/day/$variable/gn/latest/"
